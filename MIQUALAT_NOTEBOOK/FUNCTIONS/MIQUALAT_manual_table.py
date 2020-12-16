6684c610f9dfaf1c61ef48ee15f855574ce3075c
danilopignotti1987 <74015493+danilopignotti1987@users.noreply.github.com>
2020-12-16 21:24:56 +0100

    Add files via upload

--- MIQUALAT_NOTEBOOK/FUNCTIONS/MIQUALAT_manual_table.py
+++ MIQUALAT_NOTEBOOK/FUNCTIONS/MIQUALAT_manual_table.py
@@ -50,43 +50,54 @@ def relational_table_repeat():
 #relational table more genes at a time
 
 def relational_table_list_of_gene():
-	integer_progressive_ID="NULL"
-	pubmed_ID=input("enter pubmed_ID (ex 29680643), cannot be NULL (empty): ")
-	while pubmed_ID == "" or pubmed_ID.lower() == "null":
-		print ("pubmed_ID cannot be NULL retry")
-		pubmed_ID=input("enter pubmed_ID (ex 29680643), cannot be NULL (empty): ")
-	ensembl_gene_ID=input("enter ensembl_gene_ID (ex ENSBTAG00000031544,ENSBTAG00000053003), can be NULL (empty): ")
-	ensembl_gene_ID=ensembl_gene_ID.split(",")
-	ensembl_gene_ID=["NULL" if i=="" else i for i in ensembl_gene_ID]
-	ensembl_gene_ID=["NULL" if i.lower()=="null" else i for i in ensembl_gene_ID]
-	variant_name=input("enter variant_name (NULL or variant_name, variant table for more info), can be NULL (empty): ")
-	if variant_name == "" or variant_name.lower() == "null":
-		variant_name = "NULL"
-	tecnique=input("enter tecnique (ex QPCR), can be NULL (empty): ")
-	if tecnique == "" or tecnique.lower() == "null":
-		tecnique = "NULL"
-	#see for i cicle
-	keyword_tags=input("enter keyword_tags (ex apoptosis, antioxidants), insert one keyword at time, can be NULL (empty): ")
-	relationship_note=input("enter relationship_note (simple description of relationship max 200 char ex gene and tecnique), can be NULL (empty): ")
-	if relationship_note == "" or relationship_note.lower() == "null":
-		relationship_note = "NULL"
-	integer_progressive_ID=('"'+integer_progressive_ID+'"').replace(" ","")
-	pubmed_ID=('"'+pubmed_ID+'"').replace(" ","")
-	variant_name=('"'+variant_name+'"')
-	tecnique=('"'+tecnique+'"')
-	relationship_note='"'+relationship_note+'"'
-	keyword_tags=keyword_tags.split(",")
-	for i in keyword_tags:
-		if i == "" or i.lower() == "null":
-			i = "NULL"
-		i=('"'+i+'"')
-		table_file=open("PUB_GEN_VAR_TEC_TAG.csv","a")
-		for x in ensembl_gene_ID:
-			x=('"'+x+'"').replace(" ","")
-			table_file.write(integer_progressive_ID+','+pubmed_ID+','+\
-								  x+','+variant_name+','+\
-								  tecnique+','+i+','+relationship_note+'\n')            
-
+    table_file=open("PUB_GEN_VAR_TEC_TAG.csv","a")
+    entry_matrix=[]
+    integer_progressive_ID="NULL"
+    integer_progressive_ID='"'+integer_progressive_ID+'"'
+    pubmed_ID=input("enter pubmed_ID (ex 29680643 or 29680643,32359988,19164669,...), cannot be NULL (empty): ")
+    if(pubmed_ID == ""):
+        print ("\npubmed_ID cannot be  empty retry")
+    else:
+        for i,p_ID in enumerate(pubmed_ID.split(",")):
+            if(p_ID == ""):
+                print("\npubmed_ID number: "+str(i+1)+" cannot be empty retry")
+            elif(p_ID.lower() == "null"):
+                print("\npubmed_ID number: "+str(i+1)+" cannot be NULL/null retry")
+            else:
+                p_ID=('"'+p_ID+'"').replace(" ","")
+                ensembl_gene_ID=input("enter ensembl_gene_ID (ex ENSBTAG00000031544,ENSBTAG00000053003), can be NULL (empty) for pubmed_ID: "+p_ID+":").split(",")
+                ensembl_gene_ID=["NULL" if((e_ID == "") or (e_ID.lower()=="null")) else e_ID for e_ID in ensembl_gene_ID]
+                for e_ID in ensembl_gene_ID:
+                    e_ID=('"'+e_ID+'"').replace(" ","")
+                    variant_name=input("enter variant_name (NULL or BosTau11_7:62284800_A|G or BosTau11_7:62284999_A|T,BosTau11_1:3824700_C|A,...), can be NULL (empty) for ensembl_gene_ID: "+e_ID+":").split(",")
+                    variant_name=["NULL" if((name == "") or (name.lower() == "null")) else name for name in variant_name]
+                    tecnique=input("enter tecnique (ex QPCR or QPCR,Electrophoresis,...), can be NULL (empty) for ensembl_gene_ID: "+e_ID+": ").split(",")
+                    tecnique=["NULL" if((tec == "") or (tec.lower() == "null")) else tec for tec in tecnique]
+                    keyword_tags=input("enter keyword_tags (ex holstein or apoptosis,antioxidants,glutathione,...), can be NULL (empty) for ensembl_gene_ID: "+e_ID+": ").split(",")
+                    keyword_tags=["NULL" if((tag == "") or (tag.lower() == "null")) else tag for tag in keyword_tags]
+                    relationship_note=input("enter relationship_note (simple description of relationship max 200 char ex gene and tecnique), can be NULL (empty)  for ensembl_gene_ID: "+e_ID+": ").split(",")
+                    relationship_note=["NULL" if((note == "") or (note.lower() == "null")) else note for note in relationship_note]
+                    for name in variant_name:
+                        name='"'+name+'"'
+                        for tec in tecnique:
+                            tec='"'+tec+'"'
+                            for tag in keyword_tags:
+                                tag='"'+tag+'"'
+                                for note in relationship_note:
+                                    note='"'+note+'"'
+                                    entry=[]
+                                    entry.append(integer_progressive_ID)
+                                    entry.append(p_ID)
+                                    entry.append(e_ID)
+                                    entry.append(name)
+                                    entry.append(tec)
+                                    entry.append(tag)
+                                    entry.append(note)
+                                    entry_matrix.append(entry)
+    for entry in entry_matrix:
+        table_file.write(",".join(entry)+"\n")
+    table_file.close()
+    
 #tag table
   
 def tag_table():
