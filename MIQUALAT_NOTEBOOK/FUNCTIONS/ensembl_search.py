855980ec581f8b2ea77c8d5e5baf3699225f3d46
Emanuele Tufarini <emanuele.tufarini@live.com>
2020-12-16 22:33:17 +0100

    Add files via upload

--- MIQUALAT_NOTEBOOK/FUNCTIONS/ensembl_search.py
+++ MIQUALAT_NOTEBOOK/FUNCTIONS/ensembl_search.py
@@ -5,7 +5,7 @@ last modification 11-2020
 
 import ensembl_rest
 
-def ensembl_search(species,ensembl_symbol,n):
+def ensembl_search(species, ensembl_symbol, chrom_autosomal, n):
     try:
         ENSEMBL_DICT = ensembl_rest.symbol_lookup(species,ensembl_symbol)
         with open("GENE.csv","a") as GENE:
@@ -14,6 +14,15 @@ def ensembl_search(species,ensembl_symbol,n):
             if marker in DESCRIPTION:
                 ind=DESCRIPTION.index(marker)
                 DESCRIPTION=(DESCRIPTION[:ind])             
+            region_name = str(ENSEMBL_DICT.get('seq_region_name'))
+            if region_name.lower() == "x":
+                gene_name = str(int(chrom_autosomal)+1)
+            elif region_name.lower() == "y":
+                gene_name = str(int(chrom_autosomal)+2)
+            elif region_name.lower() == "mt":
+                gene_name = str(int(chrom_autosomal)+4)
+            else:
+                gene_name = region_name 
             print ('RESULT '+str(n)+' | SPECIES | ' + species +\
                    ' | SYMBOL | ' + ensembl_symbol +\
                    ' | ID | ' + str(ENSEMBL_DICT.get('id'))+\
@@ -21,20 +30,30 @@ def ensembl_search(species,ensembl_symbol,n):
                    ' | DESCRIPTION | ' + str(DESCRIPTION)+\
                    ' | ASSEMBLY_NAME | ' + str(ENSEMBL_DICT.get("assembly_name"))+\
                    ' | SPECIES | ' + str(ENSEMBL_DICT.get('species'))+\
-                   ' | SEQ_REGION_NAME | ' + str(ENSEMBL_DICT.get('seq_region_name'))+\
+                   ' | SEQ_REGION_NAME | ' + gene_name+\
                    ' | START | ' + str(ENSEMBL_DICT.get('start'))+\
                    ' | END | ' + str(ENSEMBL_DICT.get('end'))+\
-                   ' | STRAMD | ' + str(ENSEMBL_DICT.get('strand'))+'\n')
+                   ' | STRAND | ' + str(ENSEMBL_DICT.get('strand'))+'\n')
             GENE.write('"' + str(ENSEMBL_DICT.get('id'))+\
                    '","' + str(ENSEMBL_DICT.get('display_name'))+\
                    '","' + str(DESCRIPTION)+\
                    '","' + str(ENSEMBL_DICT.get("assembly_name"))+\
                    '","' + str(ENSEMBL_DICT.get('species'))+\
-                   '","' + str(ENSEMBL_DICT.get('seq_region_name'))+\
+                   '","' + gene_name+\
                    '","' + str(ENSEMBL_DICT.get('start'))+\
                    '","' + str(ENSEMBL_DICT.get('end'))+\
                    '","' + str(ENSEMBL_DICT.get('strand'))+'"\n')
     except: print("no gene were found .. for ",species,ensembl_symbol,"\n")
 
 
+'''
 
+if str(gene_name.lower()) == "x":
+    gene_name = str(chrom_autosomal+1)
+elif str(gene_name.lower()) == "y":
+    gene_name = str(chrom_autosomal+2)
+elif str(gene_name.lower()) == "mt":
+    gene_name = str(chrom_autosomal+4)
+    
+    
+'''
